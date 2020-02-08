be8f6a123af00d317ee4072d6535f66c855d8de0
Emma Wedekind <emma.wedekind@logmein.com>
2020-02-07 18:23:27 -0800

    Adding progress bar

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
+import { animated, useSpring, config } from "react-spring";
 
-const ProgressBar = () => {
+import "./progressBar.css";
+
+export default function App() {
   const [progress, setProgress] = useState("0%");
+  const props = useSpring({ width: progress, config: config.slow });
   return (
-    <div className="progress-bar">
+    <div className="App">
       <div className="button-bar">
         <button onClick={() => setProgress("0%")}>0%</button>
         <button onClick={() => setProgress("25%")}>25%</button>
-        <button onClick={() => setProgress("50%")}>50%</button>
-        <button onClick={() => setProgress("75%")}>75%</button>
-        <button onClick={() => setProgress("100%")}>100%</button>
+        <button onClick={() => setProgress("50")}>50%</button>
+        <button onClick={() => setProgress("75")}>75%</button>
+        <button onClick={() => setProgress("100")}>100%</button>
       </div>
       <div className="meter">
-        <span />
+        <animated.span style={props} />
       </div>
     </div>
   );
-};
-export default ProgressBar;
+}
